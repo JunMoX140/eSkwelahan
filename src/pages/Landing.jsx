fbe559cc4bf2d7dbc30fee9a2b59bf728ca6b2f7
Cris Vincent Ferrer <crisvinf@gmail.com>
2023-07-18 22:24:14 +0800

    Reponsive landing page test

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Eskwelahan from "../assets/images/eskwelahan.png";
-import { Carousel } from "flowbite-react";
 import { Link } from "react-router-dom";
+import DefaultCarousel from "../components/DefaultCarousel";
 
 function Landing() {
   return (
     <>
-      <div>
+    <div className="w-screen h-screen bg-red-700">
+      <div className="w-full h-full bg-gray-700">
+       <div className="w-full bg-color1" style={{height:"8%"}}>
+          <img className="h-full" src={Eskwelahan} alt="" />
+       </div>
+       <div className="bg-color2 w-full" style={{height:"92%"}}>
+        <div className="w-1/2 bg-color5 h-full">
+            <div className="flex h-1/2 w-full justify-center items-end bg-green-700">
+              <div className="text-center text-2xl">FREE Class Management System
+                <br />
+                for everyone!</div>
+            </div>
+          </div>
+          <div className="w-1/2 h-full">
+
+          </div>
+       </div>
+       <div >
+
+       </div>
+      </div>
+    </div>
+      {/* <div>
         <div
-          className="h-screen w-full "
+          className="h-screen w-full"
           style={{
             background:
               "linear-gradient(0deg, #D81159 0%, #D81159 100%), linear-gradient(180deg, #218380 0%, rgba(33, 131, 128, 0) 40%)",
@@ -22,8 +44,8 @@ function Landing() {
                 style={{
                   right: 150,
                   top: 0,
-                  width: "764px",
-                  height: "335px",
+                  width: "40%",
+                  height: "30%",
                   position: "absolute",
                 }}
               />
@@ -48,7 +70,6 @@ function Landing() {
                   Login
                 </button>
               </Link>
-              {/* <div style={{width: 400, height: 21, right: 375, top: 760, position: 'absolute', textAlign: 'right', color: '#FFBC42', fontSize: 20, fontFamily: 'Monospace', fontWeight: '300', textDecoration: 'underline', wordWrap: 'break-word'}}>Don't have an account? Sign Up</div> */}
               <div
                 style={{
                   width: 734,
@@ -134,7 +155,7 @@ function Landing() {
                   borderRadius: 9999,
                 }}
               />
-
+              
               <div
                 style={{
                   width: 600,
@@ -146,64 +167,18 @@ function Landing() {
                   borderRadius: 20,
                 }}
               >
-                <Carousel>
-                  <img
-                    alt="..."
-                    src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-                  />
-                  <img
-                    alt="..."
-                    src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-                  />
-                  <img
-                    alt="..."
-                    src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-                  />
-                  <img
-                    alt="..."
-                    src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-                  />
-                  <img
-                    alt="..."
-                    src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-                  />
-                </Carousel>
+                <DefaultCarousel />
+                
               </div>
             </div>
           </div>
 
-          {/* <div className='h-full w-full grid grid-cols-2'>
-         <div className="flex items-center justify-center col-span-1 grid grid-rows-2" >
-          <div className=""></div>
-          <div className="">
-            <button className="bg-color5 text-color1 text-xl" style={{width: 383, height: 98, left: 151, top: 754, position: 'absolute', textAlign: 'center', color: '#D81159', fontSize: 48, fontFamily: 'Inconsolata', fontWeight: '700', wordWrap: 'break-word'}}>Login</button>
-            </div>
-         </div>
-        </div> */}
         </div>
-      </div>
+      </div> */}
 
-      {/* <div style={{width: '100%', height: '100vh', background: 'linear-gradient(0deg, #D81159 0%, #D81159 100%), linear-gradient(180deg, #218380 0%, rgba(33, 131, 128, 0) 40%)'}}>
-  <img className="w-[764px] h-[335px]" src="https://via.placeholder.com/764x335" />
-</div> */}
+     
 
-      {/* <div style={{width: 1440, height: 1024, position: 'relative', background: 'white'}}>
-  <div style={{width: 1440, height: 1024, left: 0, top: 1024, position: 'absolute', background: 'linear-gradient(0deg, #D81159 0%, #D81159 100%), linear-gradient(180deg, #218380 0%, rgba(33, 131, 128, 0) 40%)'}} />
-  <div style={{width: 445, height: 445, left: 1066, top: -63, position: 'absolute', background: '#FFBC42', borderRadius: 9999}} />
-  <div style={{width: 432, height: 400, left: 960, top: 624, position: 'absolute', background: '#FFBC42', borderRadius: 9999}} />
-  <div style={{width: 148, height: 148, left: 1244, top: 405, position: 'absolute', background: '#FFBC42'}}></div>
-  <div style={{width: 145, height: 149, left: 814, top: 848, position: 'absolute', background: '#FFBC42'}}></div>
-  <img style={{width: 764, height: 335, left: 14, top: 47, position: 'absolute'}} src="https://via.placeholder.com/764x335" />
-  <div style={{width: 533, height: 667, left: 785, top: 170, position: 'absolute', background: '#D9D9D9', borderRadius: 8}}></div>
-  <div style={{left: 922, top: 453, position: 'absolute', color: '#FFBC42', fontSize: 48, fontFamily: 'Inconsolata', fontWeight: '700', wordWrap: 'break-word'}}>img carousel<br/>here</div>
-  <div style={{width: 383, height: 98, left: 151, top: 754, position: 'absolute'}}>
-    <div style={{width: 383, height: 98, left: 0, top: 0, position: 'absolute', background: '#FFBC42', borderRadius: 8}} />
-    <div style={{left: 119, top: 24, position: 'absolute', textAlign: 'center', color: '#D81159', fontSize: 48, fontFamily: 'Inconsolata', fontWeight: '700', wordWrap: 'break-word'}}>LOG IN</div>
-  </div>
-  <div style={{width: 684, height: 257, left: 54, top: 608, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inconsolata', fontWeight: '700', wordWrap: 'break-word'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-  <div style={{width: 298, height: 21, left: 181, top: 902, position: 'absolute', textAlign: 'right', color: '#FFBC42', fontSize: 20, fontFamily: 'Roboto', fontWeight: '300', textDecoration: 'underline', wordWrap: 'break-word'}}>Don't have an account? Sign Up</div>
-  <div style={{width: 734, height: 132, left: 4, top: 382, position: 'absolute', textAlign: 'center', color: '#D9D9D9', fontSize: 48, fontFamily: 'Source Code Pro', fontWeight: '700', wordWrap: 'break-word'}}>FREE Class Management System<br/>for everyone!</div>
-</div> */}
+      
     </>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -67,35 +67,6 @@ function Register() {
                 <a className='w-full text-blue-700 underline' href="/login">Already have and account?Sign in</a>
               </div>
             </div>
-            {/* <div className="flex flex-wrap -mx-3 mb-2">
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                  City
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque"/>
-              </div>
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                  State
-                </label>
-                <div className="relative">
-                  <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                  Zip
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210"/>
-              </div>
-            </div> */}
           </form>
           </div>
         </div>
